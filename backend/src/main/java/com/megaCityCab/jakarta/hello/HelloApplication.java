3df3bed3840f049a67efcc9eef00e638310973cd
Chamod Kalpitha <chamodlive@live.com>
2025-02-05 16:05:33 +0530

    Add initial backend structure with Jakarta EE setup and configuration files

---
+++ backend/src/main/java/com/megaCityCab/jakarta/hello/HelloApplication.java
@@ -0,0 +1,8 @@
+package com.megaCityCab.jakarta.hello;
+
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+
+@ApplicationPath("rest")
+public class HelloApplication extends Application {
+}
